340a7c5748ea8f7d682b306309950aa67699e4a3
Дмитрий <davydo44@yandex.ru>
2026-09-08 11:38:30 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -522,7 +522,6 @@
       const { width, height, percent } = readSettings();
       els.widthInput.value = String(width);
       els.heightInput.value = String(height);
-      const { width, height, percent } = readSettings();
       newGame(width, height, percent);
       setSettingsVisible(false);
       showToast("Новая игра: " + width + "×" + height, null);
